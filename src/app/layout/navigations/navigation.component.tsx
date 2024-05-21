c68e92de9be118e7d36b41f8cd6575a06a0e420b
Moniroth-03 <127386659+Moniroth-03@users.noreply.github.com>
2024-05-21 17:47:56 +0700

    completed navigation link

--- src/app/layout/navigations/navigation.component.tsx
+++ src/app/layout/navigations/navigation.component.tsx
@@ -1,9 +1,11 @@
 import { NavLink } from "react-router-dom";
 import { FaDesktop } from "react-icons/fa6";
+import { FaFileInvoiceDollar } from "react-icons/fa6";
+import { FaFileInvoice } from "react-icons/fa";
 let arr = [
-  { name: "Dashboard", path: "/dashboard", icon:<FaDesktop /> },
-  { name: "Orders", path: "/orders" },
-  { name: "OrdersDetail", path: "/ordersDetail" },
+  { name: "Dashboard", path: "/dashboard", icon: <FaDesktop /> },
+  { name: "Orders", path: "/orders", icon: <FaFileInvoiceDollar /> },
+  { name: "OrdersDetail", path: "/ordersDetail", icon: <FaFileInvoice /> },
 ];
 
 const NavigationComponent = () => {
@@ -15,7 +17,7 @@ const NavigationComponent = () => {
           className={({ isActive }) =>
             isActive
               ? "bg-gradient-to-r from-green-200 to-blue-300 text-white p-2 rounded-md flex gap-3 items-center"
-              : ""
+              : " flex gap-3 items-center p-2"
           }
         >
           {obj.icon} {obj.name}
